be8c3406ef085da590e66b24532be3d46f7652a0
Dmitry Razinkov <razinkov_dmitrij@mail.ru>
2020-10-25 13:23:11 +0300

    Moved repo

--- src/lang/i18n.ts
+++
@@ -1,7 +0,0 @@
-import { createI18n } from "vue-i18n";
-import { messages } from "@/lang/index";
-
-export default createI18n({
-  locale: "en-US",
-  messages
-});
--- src/vue-component-types.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from "vuex";
-import { Router } from "vue-router";
-import { RootState } from "@/store/types";
-
-export interface InstalledPlugins {
-  $store: Store<RootState>;
-  $router: Router;
-}
